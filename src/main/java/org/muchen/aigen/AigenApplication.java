c9d759f01287b5b71f5966ac0c5a414155ebdaf3
mu-chentx <2605167612@qq.com>
2025-11-25 00:10:45 +0800

    第一次提交

---
+++ src/main/java/org/muchen/aigen/AigenApplication.java
@@ -0,0 +1,13 @@
+package org.muchen.aigen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AigenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AigenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/muchen/aigen/AigenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.muchen.aigen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AigenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
